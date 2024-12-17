b1a0940181ba0f5dde24766ebe9835d93501fd50
DarKrone <Brener-2000@mail.ru>
2024-12-17 17:18:30 +0400

    Fix 404 on refreshing page

--- aspreacttodo.client/src/App.jsx
+++ aspreacttodo.client/src/App.jsx
@@ -1,19 +1,19 @@
 import './App.css';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home.jsx'
 import Login from './Pages/Login.jsx'
 import Register from './Pages/Register.jsx'
 
 function App() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/" element={<Home /> } />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
         )
 }
 
